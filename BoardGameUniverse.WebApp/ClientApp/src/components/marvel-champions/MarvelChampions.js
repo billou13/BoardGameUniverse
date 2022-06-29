639b3a5c4016478579d46b69a2fa41e01ef21bdc
billou13 <billou13@gmail.com>
2022-06-29 22:19:24 +0200

    add card

--- BoardGameUniverse.WebApp/ClientApp/src/components/marvel-champions/MarvelChampions.js
+++ BoardGameUniverse.WebApp/ClientApp/src/components/marvel-champions/MarvelChampions.js
@@ -13,6 +13,7 @@ export class MarvelChampions extends Component {
             <li><Link to="/marvel-champions/card/01001a">Spider-Man</Link></li>
             <li><Link to="/marvel-champions/card/01052">Chase Them Down</Link></li>
             <li><Link to="/marvel-champions/card/01074">Inspired</Link></li>
+            <li><Link to="/marvel-champions/card/01092">Helicarrier</Link></li>
           </ul>
         </div>
       </div>
